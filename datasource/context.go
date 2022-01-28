1b50446a0b13c2149a38bcf4596246e9be06a799
hongbo_fu <hongbo.fu@cn.abb.com>
2022-01-28 16:30:58 +0800

    fix

--- datasource/context.go
+++ datasource/context.go
@@ -204,7 +204,7 @@ func (m ContextSimple) Get(key string) (value.Value, bool) {
 func getVaule(v map[string]value.Value, key string) (value.Value, bool) {
 	left, right, hasNamespace := expr.LeftRight(key)
 	if !hasNamespace {
-		return v[key], false
+		return v[key], true
 	}
 	val, ok := v[left]
 	if !ok {
